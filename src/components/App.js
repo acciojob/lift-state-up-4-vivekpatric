c562ba825afce1bc8193a9dbaa7bb2ab16c02c3e
Vivek Patric R <96299357+vivekpatric@users.noreply.github.com>
2023-07-19 14:50:09 +0000

    result

--- src/components/App.js
+++ src/components/App.js
@@ -2,10 +2,12 @@
 import React from "react";
 import './../styles/App.css';
 import Parent from "./Parent";
+
 const App = () => {
   return (
     <div>
-       <Parent />
+        {/* Do not remove the main div */}
+        <Parent />
     </div>
   )
 }
--- src/components/Child.js
+++ src/components/Child.js
@@ -1,3 +1,4 @@
+import React from "react";
 const Child = ({ items, changeState }) => {
 
     return (
